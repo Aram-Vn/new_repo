a7d88f32b31a4f2dec67d9dfae1434dc9432a15e
Aram <aramvardanyan13@gmail.com>
2023-11-15 17:50:40 +0400

    foo1

--- 1__fact.cpp
+++ 1__fact.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int fib(int num);
+int foo1(int num);
 int foo(int num);
 
 int main(){
@@ -9,7 +9,7 @@ int main(){
  std::cin >> num;
   
  std::cout << foo(num) << std::endl;
- std::cout << fib(num) << std::endl;
+ std::cout << foo1(num) << std::endl;
 
 }
 
@@ -23,7 +23,7 @@ int foo(int num){
 	return res;
 }
 
-int fib(int num){
+int foo1(int num){
 	int tmp = 0;
 	int last = 0;
 	int next = 1;
