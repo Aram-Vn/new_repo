154e6a097b62b8057735476ce91dc818776f4897
Aram <aramvardanyan13@gmail.com>
2023-11-27 19:35:17 +0400

    a

--- 06_C++_string/main.cpp
+++ 06_C++_string/main.cpp
@@ -10,26 +10,26 @@ int main(){
  Str s1("qwerertyutqwertqwertwertertyer we rty");
  /* s1.print(); */
 
- Str f1 = s;
- f1.print(); // Segmentation fault (core dumped) 
+ /* Str f1 = s; */
+ // f1.print();   // Segmentation fault (core dumped)  
 
  /* Str f2 = s1; */
- /* f2.print(); */
+ //f2.print();  // Segmentation fault (core dumped) 
 
  
  /* std::cout << "\n*******" << std::endl; */
 
- /* Str a; */
+ Str a;
 
- /* a = s1 + s; */	
- /* a.print(); */ 
+ a = s1 + s;	
+ a.print();  // works!!!!!
  
  /* std::cout << "\n*******" << std::endl; */
  
  /* Str a1; */
 
  /* a1 = s + s1; */	
- /* a1.print(); */ 
+ //a1.print(); // Segmentation fault (core dumped) 
  
 
 }
